ecd0f81185b12a23022066093760f5e9633c5f1a
ImboredTech <134144540+OpenLightPlugin@users.noreply.github.com>
2023-06-09 23:18:02 +0100

    Update moderation.py
    Added 2 new commands, kick which just kicks the specified member from the server as long as the command runner and the bot has the correct permission.
    Purge deletes the specified amount of messages in the channel that the command is ran in as long as it is not over 100 messages and the messages aren't over 14 days old.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,6 +5,7 @@ import time
 today = date.today()
 import datetime
 from utils.util import Pag
+import asyncio
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -77,6 +78,65 @@ class Moderation(commands.Cog):
             await ctx.send("I do not have the right permissions!\nI am missing the `ban_members` permission.")
         else:
             print(error)
+
+    @commands.slash_command(name="kick", description="Kicks the specified member for the specified reason!")
+    @commands.has_guild_permissions(kick_members=True)
+    @commands.bot_has_guild_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.SlashCommandOptionType.user, reason : discord.Option(str, required=False)):
+        await ctx.defer()
+        if reason == None:
+            reason = "No Reason Specified"
+        embed = discord.Embed(title="Member Kicked", description="I have successfully kicked the specified member!", color=discord.Color.green())
+        embed.add_field(name="Member Name: ", value=f"{member.mention}", inline=False)
+        embed.add_field(name="Reason: ", value=f"{reason}", inline=False)
+        embed.set_footer(text=f"Requested By: {ctx.author.name}", icon_url=ctx.author.avatar)
+        memberEmbed = discord.Embed(title="You have been kicked", description=f"You have been kicked from the server {ctx.guild.name}", color=discord.Color.red())
+        memberEmbed.add_field(name="Responsible Moderator: ", value=f"{ctx.author.mention}", inline=False)
+        memberEmbed.add_field(name="Reason: ", value=f"{reason}", inline=False)
+        memberEmbed.add_field(name="Date: ", value=f"{today.day}-{today.month}-{today.year}")
+        kick_filter = {"user_id": member.id, "guild_id": ctx.guild.id}
+        kick_data = {"reason": reason, "timestamp": time.time(), "moderator": ctx.author.id}
+
+        await self.bot.kicks.upsert_custom(kick_filter, kick_data)
+        await ctx.followup.send(Embed=embed)
+        try:
+            await member.send(embed=memberEmbed)
+            await member.kick()
+        except discord.HTTPException:
+            await member.kick()
+            await ctx.send("Member has not been notifed about their kick due to their DMs being turned off.")
+    
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You do not have permission to run this command!\nYou are missing the `kick_members` permission.")
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send("I do not have the right permissions!\nI am missing the `kick_members` permission.")
+        else:
+            print(error)
+
+    @commands.slash_command(name="purge", description="Purges an specified amount of messages, max messages is 100.")
+    @commands.has_guild_permissions(manage_messages=True)
+    @commands.bot_has_guild_permissions(manage_messages=True)
+    async def purge(self, ctx, amount : discord.SlashCommandOptionType.integer):
+        await ctx.defer()
+        if amount > 100:
+            await ctx.followup.send("You can only delete 100 messages or less.")
+            return
+        if amount < 0:
+            await ctx.followup.send("You can not delete negative amount of messages.")
+            return
+        if amount == 0:
+            await ctx.followup.send("You can not delete 0 messages.")
+            return
+        embed = discord.Embed(title="Messages Deleted", description=f"I have successfully deleted {amount} messages.\n*I delete after 5 seconds*", color=discord.Color.green())
+        embed.set_footer(text=f"Requested By: {ctx.author.name}", icon_url=ctx.author.avatar)
+        amount = amount + 1
+        try:
+            await ctx.channel.purge(limit=amount)
+            await ctx.send(embed=embed, delete_after=5)
+        except discord.HTTPException:
+            await ctx.send("You are trying to delete messages that are over 14 days old.")
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
