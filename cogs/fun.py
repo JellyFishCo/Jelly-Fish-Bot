1c36f4e79b9236c228d7e057f596199901902a21
ImboredTech <134144540+OpenLightPlugin@users.noreply.github.com>
2023-07-01 14:46:16 +0100

    Update cogs/fun.py
    Added a new game tictactoe. This game requires another player and can not be played with the bot.

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord_games.button_games import BetaRockPaperScissors
 from discord_games.hangman import Hangman
+from discord_games.tictactoe import Tictactoe
 
 class Games(commands.Cog):
     def __init__(self, bot):
@@ -12,10 +13,16 @@ class Games(commands.Cog):
         game = BetaRockPaperScissors(player)
         await game.start(ctx)
 
-    @commands.slash_command(name="hangman", description="Starts up a game of Hangman")
+    @commands.slash_command(name="hangman", description="Starts up a game of Hangman.")
     async def hangman(self, ctx: commands.Context[commands.Bot]):
         game = Hangman()
         await game.start(ctx, delete_after_guess=True)
+
+    @commands.slash_command(name="tictactoe", description="Starts up a game of TicTacToe.")
+    async def tictactoe(self, ctx: commands.Context[commands.Bot], player: discord.Option(discord.SlashCommandOptionType.user, required=True, description="The other person you want to play with")):
+        game = Tictactoe(cross=ctx.author, circle=player)
+        await game.start(ctx)
+    
 
 def setup(bot):
     bot.add_cog(Games(bot))
